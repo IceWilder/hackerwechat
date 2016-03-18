5a00bcca74c7ab74b00536f02af70d7b81afe421
darcygail <darcy.cj51@gmail.com>
2016-03-18 15:09:10 +0800

    delete local Model

--- server/Application/Admin/Controller/ReportController.class.php
+++ server/Application/Admin/Controller/ReportController.class.php
@@ -13,5 +13,13 @@ use Think\Controller;
 
 class ReportController extends Controller
 {
-
+    public function delete()
+    {
+        $report_id = I('report_id');
+        if ($report_id) {
+            $grModel = M('group_report');
+            $grModel->del_flag = '0';
+            $grModel->where('report_id=' . $report_id)->save();
+        }
+    }
 }
