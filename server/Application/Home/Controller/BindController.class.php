48cc0dd041afddfb240e13a451f6f530969c9bc2
darcygail <darcy.cj51@gmail.com>
2016-03-18 17:48:18 +0800

    bind function finished

--- server/Application/Home/Controller/BindController.class.php
+++ server/Application/Home/Controller/BindController.class.php
@@ -13,5 +13,33 @@ use Think\Controller;
 
 class BindController extends Controller
 {
-
+    public function index()
+    {
+        if (IS_POST) {
+            $hacker_id = $_POST['hacker_id'];
+            $mobile = $_POST['mobile'];
+            if ($hacker_id && $mobile) {
+                $data = explode('#', $hacker_id);
+                $where = array(
+                    'wechatID' => $data[0],
+                    'group_id' => $data[1],
+                );
+                $groupModel = M('group');
+                $result = $groupModel->where($where)->find();
+                if (!$result) {
+                    returnApiError('并没有这样的hacker_id');
+                    die();
+                }
+                $userModel = M('user');
+                $user_id = $userModel->getFieldByMobile($mobile);
+                if (!$user_id) {
+                    returnApiError('尚未加入团队');
+                    die();
+                }
+                $userModel->is_bind = 1;
+                $userModel->where('id=' . $user_id)->save();
+                returnApiSuccess('绑定成功');
+            }
+        }
+    }
 }
