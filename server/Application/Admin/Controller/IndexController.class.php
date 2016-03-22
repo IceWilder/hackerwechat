2f9f760ee71d234f763d9f409740398466bd274b
darcygail <darcy.cj51@gmail.com>
2016-03-22 21:59:12 +0800

    add backgroud template

---
+++ server/Application/Admin/Controller/IndexController.class.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: darcygail
+ * Date: 3/22/16
+ * Time: 9:47 PM
+ */
+
+namespace Admin\Controller;
+
+
+use Think\Controller;
+
+class IndexController extends Controller
+{
+    public function index()
+    {
+        $this->display();
+    }
+}
